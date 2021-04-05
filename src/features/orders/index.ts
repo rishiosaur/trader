fec7fbbbf98fb38c08b35faec0bdfd281153a4bf
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 12:10:58 -0400

    ephemeral

--- src/features/orders/index.ts
+++ src/features/orders/index.ts
@@ -506,7 +506,7 @@ const orderController = (app: App) => {
 
 		const orders = unfiltered.filter((z) => z.approved)
 
-		await postMessage(body.channel_id, [
+		await postEphemeral(body.channel_id, body.user_id,[
 			{
 				type: 'header',
 				text: {
