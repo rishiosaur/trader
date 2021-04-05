03698a806f9e3941f4521cf8237782904be44f77
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:03:50 -0400

    config!

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
-export const signing_secret = process.env.signing_secret;
-export const token = process.env.token;
-export const name = "Hello World!"
+/* eslint-disable prefer-destructuring */
+export const signing_secret = process.env.signing_secret
+export const hn_api = process.env.hn
+export const market_api = process.env.market
+export const hn_token = process.env.hn_token
+export const market_token = process.env.market_token
+export const token = process.env.token
+export const name = 'Villager'
+export const marketStreamChannel = process.env.market_stream_channel
+export const marketReviewChannel = process.env.market_review_channel
