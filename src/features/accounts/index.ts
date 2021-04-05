b4505677059dbf7aa007693649b7c6940de62cb2
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 11:20:37 -0400

    start message

--- src/features/accounts/index.ts
+++ src/features/accounts/index.ts
@@ -20,7 +20,12 @@ const accountController = (app: App) => {
 
 		if (userCreated) {
 			await sayEphemeral(
-				...blocksAndText("I've created an account for you. Happy selling!")
+				...blocksAndText(`I've created an account for you. Happy selling! Here's a bunch of stuff that you can do with the Market:
+- \`/market-create-order\` - here’s where you can make an order (an order is just a product that you can sell in the market)!
+- \`/market-rate-seller\` & \`/market-rate-buyer\` - to ensure quality, everything that goes on in the market goes through checking; if you ever feel like it, make sure to rate your buying or selling experience with other people using these commands!
+- \`/market-user <user>\` (example: \`/market-user <@UHFEGV147>\`) - to see the ratings, products sold (and selling), and more information about a person, use this command!
+- \`/market-order <id>\` - Oftentimes, the information provided at a glance isn’t enough; use this command to buy a product or view more granular data about it!
+For more information, make sure to check <#C01TNJ9NDLH> and the <https://hackclub.slack.com/archives/C0M8PUPU6/p1617634192356300|ship>!`)
 			)
 			return
 		}
