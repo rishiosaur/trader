9ed1cb44eceebe9b2e3ada5858938b0b34e1e20c
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:04:32 -0400

    remove feature

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,3 +1,4 @@
 // This is where you should export all the features. Every folder in the starter kit follows this spec.
 
-export { default as feature1 } from './feature1';
+export { default as accountController } from './accounts'
+export { default as orderController } from './orders'
